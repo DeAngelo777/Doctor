a8905f956fd6e9164377e25c4569dbe91ee9f1ba
tomaskls <tomaskls@hotmail.com>
2024-09-20 11:57:05 +0300

    header

--- src/assets/components/Firstsection.jsx
+++ src/assets/components/Firstsection.jsx
@@ -5,6 +5,12 @@ export function Firstsection() {
 
     return (
       <>
+      <div>
+    <div></div>
+    <div>
+        <img src="" alt="" />
+    </div>
+</div>
         
       </>
     )
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,10 +1,22 @@
-import  './Firstsection.css'
+import './Firstsection.css'
 
 export function Header() {
 
     return (
-      <>
-        
-      </>
+        <>
+            <div className='header'>
+                <h2 className='headerh2'>Doctor<span className='care'>Care</span></h2>
+                <div className='headerLink'>
+
+                    <a className='headerLinkai' href="">Inicio</a>
+                    <a className='headerLinkai' href="">Sobre</a>
+                    <a className='headerLinkai' href="">Serviqos</a>
+                    <a className='headerLinkai' href="">Depoimentos</a>
+                </div>
+                <div>
+                    <button className='headerBtn'>AGENDAR CONSULTA</button>
+                </div>
+            </div>
+        </>
     )
-  }
+}
